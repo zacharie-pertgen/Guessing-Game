b2e750ee10aa2cf64d7d68d6fc089eb9812ed8be
zacharie-pertgen <zachpertgen007@gmail.com>
2018-05-09 13:53:18 -0700

    Add logic and formatting
    Added game replay mechanics, and cleaned up formatting so that the screen will wipe. Also added array of previous guesses and a range to stay inside, so that numbers cannot be guessed multiple times.

--- guessGame.py
+++ guessGame.py
@@ -2,20 +2,45 @@
 # Developed by Max Pertgen
 # v1.0 5/9/18
 
-
+import os
 import random
+import time
 
-guesses = 0
-number = random.randint(1, 10)
 
-print("I'm thinking of a number between 1 and 100! Can you guess it?")
+while True:
+    numOfGuesses = 0
+    number = random.randint(1, 2)
+    guesses = []
+    guesses_set = set(guesses)
 
-while guesses < 10:
+    os.system('cls')
+    print("I'm thinking of a number between 1 and 10! Can you guess it?")
 
-    theGuess = int(input("Your guess: "))
-    if theGuess == number:
-        print("Congratulations! You guessed the number in " + str(guesses) + " guesses!")
-        break
-    else:
-        print("That is incorrect :( Please try again!")
-        guesses += 1
+    while numOfGuesses < 10:
+        theGuess = int(input("Your guess: "))
+        if theGuess in guesses:
+            os.system('cls')
+            print("Sorry, you already guessed that number! Try again!")
+        elif theGuess not in range(1, 11):
+            os.system('cls')
+            print("Sorry, your guess is not between 1-10! Try again.")
+            print("So far you have guessed: " + ",".join(map(str,guesses)))
+        else:
+            if theGuess == number:
+                os.system('cls')
+                print("Congratulations! You guessed the number in " + str(numOfGuesses) + " guesses!")
+                replay = input("Would you like to play again?: ")
+                if replay == 'yes' or 'Yes':
+                    print("Great! Here we go again!")
+                    time.sleep(2)
+                    break
+                if replay == 'no' or 'No':
+                    print ("Goodbye!")
+                    time.sleep(2)
+                    raise SystemExit
+            else:
+                guesses.append(theGuess)
+                os.system('cls')
+                print("That is incorrect :( Please try again!")
+                print("So far you have guessed: " + ",".join(map(str,guesses)))
+                numOfGuesses += 1   
